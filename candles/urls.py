6a47e88b9f4510e6a6fefc75c84be4324a671506
Yuffi64 <100430798+Yuffi64@users.noreply.github.com>
2022-03-16 11:32:00 -0500

    Created new view function for getting candles by ID + added new URLs path for candle detail view function.

--- candles/urls.py
+++ candles/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns =[
     path('', views.fragrant_list),
+    path('<int:pk>/', views.fragrant_detail)
 ]
--- candles/views.py
+++ candles/views.py
@@ -1,21 +1,41 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import FragrantSerializer 
 from .models import Fragrant
-from candles import serializers
 
 
 
 
 
-
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def fragrant_list(request):
-    fragrant = Fragrant.objects.all()
     
-    serializers = FragrantSerializer(fragrant, many=True)
+    if request.method == 'GET':
+        fragrant = Fragrant.objects.all()
+        serializers = FragrantSerializer(fragrant, many=True) 
+        return Response(serializers.data)
     
     
     
+    elif request.method == 'POST':
+        serializer = FragrantSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+       
+       
+             
+@api_view(['GET'])
+def fragrant_detail(request, pk):
+    try:
+        fragrant = Fragrant.objects.get(pk=pk)
+        serializer = FragrantSerializer(fragrant);
+        return Response(serializer.data)
     
-    return Response(serializers.data)
+    
+    
+    except Fragrant.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND);
+    
+     
